d1475baeef21be336de3812a15cb9322428c1054
tschroeer72 <t.schroeer@web.de>
2026-03-13 09:00:20 +0100

    Dokploy Test

--- KEPABackend/Program.cs
+++ KEPABackend/Program.cs
@@ -111,11 +111,11 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
+if (app.Environment.IsDevelopment())
+{
      app.UseSwagger();
      app.UseSwaggerUI();
-// }
+}
 
 
 app.MapControllers();
